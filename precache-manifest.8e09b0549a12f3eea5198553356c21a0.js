0846ab25a71def612a41318ffbdb4ea02c763346
your <leonardoxrrp@gmail.com>
2020-10-13 17:15:57 +0200

    Updates

---
+++ precache-manifest.8e09b0549a12f3eea5198553356c21a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71001e27a2f41ac19558b3780f4bb984",
+    "url": "/reacttodolist/index.html"
+  },
+  {
+    "revision": "e7257b330b9ac64ab8b3",
+    "url": "/reacttodolist/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f6b50ec4a39fb408e95e",
+    "url": "/reacttodolist/static/css/main.56893215.chunk.css"
+  },
+  {
+    "revision": "e7257b330b9ac64ab8b3",
+    "url": "/reacttodolist/static/js/2.548a77e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodolist/static/js/2.548a77e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b50ec4a39fb408e95e",
+    "url": "/reacttodolist/static/js/main.eb7739ed.chunk.js"
+  },
+  {
+    "revision": "afb45d3d2075ce3bcfa0",
+    "url": "/reacttodolist/static/js/runtime-main.ce85a729.js"
+  }
+]);
